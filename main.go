707f438b25d2a3a3cdf34449e53ac9a2ee2ed3d1
Thomas Fan <thomasfan100@gmail.com>
2021-07-26 10:52:08 -0500

    .

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -19,6 +20,10 @@ var errReadingInput = errors.New("Error reading input")
 func main() {
 
 	for {
+		if len(os.Args) != 2 {
+			printError(errInvalidArguments)
+		}
+		originUnit = strings.ToUpper(os.Args[1])
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
